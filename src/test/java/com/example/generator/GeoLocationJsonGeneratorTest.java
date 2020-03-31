acb13fb9ed4724cd77eac17348878c1de62e667e
Marcin <marcin.bordowicz2@gmail.com>
2020-03-31 10:33:00 +0200

    improve  tests class

--- src/test/java/com/example/generator/GeoLocationJsonGeneratorTest.java
+++ src/test/java/com/example/generator/GeoLocationJsonGeneratorTest.java
@@ -2,11 +2,13 @@ package com.example.generator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GeoLocationJsonGeneratorTest {
 
+    @Autowired
     private GeoLocationJsonGenerator geoLocationJsonGenerator;
 
     @Test
